2d02620cf358da78e91a00ab63f18ce069d72852
Jack Kuan <kjkuan@gmail.com>
2025-03-18 20:50:13 +0000

    Use git for versioning wiki files and other minor fixes.

---
+++ examples/wiki-app/Index.md
@@ -0,0 +1,27 @@
+# Wiki
+
+This is the default starting page of the wiki. You can edit it or create a new page.
+To link to a new or existing page, simply use the link syntax in Markdown, like:
+
+```markdown
+    This is a [link](relative-path-to-another-file) to another page.
+```
+
+Which, in preview-mode, would look like:
+
+>    This is a [link](relative-path-to-another-file) to another page.
+
+If the link URL is omitted in the parentheses (`()`) then it defaults to the display name
+specified in the square brackets (`[...]`).
+
+Clicking on a link navigates to the page (view) if it exists. If the page doesn't already exists,
+it gets created and the app lets you edit it right then.
+
+When editing a page, you may click the `Preview` button anytime to see how it will be rendered;
+clicking the `Save` button to save the page and return to the page view mode.
+
+Clicking on a link with an external URL will open the page in a separate window.
+
+You can drag and drop files or directories in the file explorer panel on the left to
+organize your pages. Right clicking on the panel opens a context menu that allows you
+to delete and rename selected files.
--- examples/wiki-app/main.sh
+++ examples/wiki-app/main.sh
@@ -3,8 +3,12 @@
 # A simple personal wiki app based on Markdown
 #
 # TODO:
-#  - Use git for versioning the wiki files
 #  - Allow tagging a document
+#    - Use this syntax in the doc:  [//]: # (tag tag1 tag2 tag3 ...)
+#    - the same mechanism can be used for embedding other metadata. E.g.
+#      - [//]: # (title "Document Title")
+#      - [//]: # (author "First Last")
+#
 #  - Allow incremental searching
 #  - Generate backlinks
 #  - Generate knowledge graph
@@ -12,59 +16,64 @@
 #    - Generate a ToC
 #  - Allow transclusion
 #  - Implement group operations?
-#
+#    - allow selecting multiple files
 set -eo pipefail
 
 source baguette.sh
 
-cd "$WSD_SCRIPT_DIR"
+FE_LS_OPTS+=(--ignore=.*)
+FE_MODEL=  # to be created in @main
 
-WIKI_HOME=$PWD/wiki
+WIKI_HOME=${WIKI_HOME:-$WSD_SCRIPT_DIR/wiki}
 
-[[ -d $WIKI_HOME ]] || {
-    mkdir -p "$WIKI_HOME"
-    cat > "$WIKI_HOME/Index.md" <<'EOF'
-# Wiki
+CURRENT_NODE=  # path-id of the current node being viewed
+CURRENT_CONTENTS=
 
-This is the default starting page of the wiki. You can edit it or create a new page.
-To link to a new or existing page, simply use the link syntax in Markdown, like:
+# a stack of path-id's that lead to the currently selected page being viewed;
+# ${LINK_STACK[-1]} is the top.
+#
+LINK_STACK=("")
 
-```markdown
-    This is a [link](relative-path-to-another-file) to another page.
-```
+class WikiFEModel isa=FileExplorerModel
 
-Which, in preview-mode, would look like:
-
->    This is a [link](relative-path-to-another-file) to another page.
-
-If the link URL is omitted in the parentheses (`()`) then it defaults to the display name
-specified in the square brackets (`[...]`).
-
-Clicking on a link navigates to the page (view) if it exists. If the page doesn't already exists,
-it gets created and the app lets you edit it right then.
-
-When editing a page, you may click the `Preview` button anytime to see how it will be rendered;
-clicking the `Save` button to save the page and return to the page view mode.
-
-Clicking on a link with an external URL will open the page in a separate window.
-
-You can drag and drop files or directories in the file explorer panel on the left to
-organize your pages. Right clicking on the panel opens a context menu that allows you
-to delete and rename selected files.
-EOF
+# Hide the .md extension when listing files
+WikiFEModel/list () {
+    msg $self ../list "$@"
+    local i
+    for ((i=0; i < ${#RESULT[*]}; i++)); do
+        RESULT[i]=${RESULT[i]%.md}
+    done
 }
+WikiFEModel/has-path-id () { [[ ${1:-} ]] && [[ ${self[$1]:-} ]]; }
 
-declare -g FE_MODEL
-FileExplorerModel FE_MODEL root="$WIKI_HOME"
-
+git-tracked () { git ls-files --error-unmatch -- "${1:?}" >/dev/null 2>&1; }
 
 @main () {
-    main/ id=main data-scope
+    local new_wiki_home
+    [[ -d $WIKI_HOME ]] || {
+        mkdir -p "$WIKI_HOME"; (
+            cd "$WIKI_HOME"
 
+            git init -b main
+            git config user.email "baguette+wiki@localhost"
+            git config user.name "Baguette Wiki-App"
+
+            cp "$WSD_SCRIPT_DIR/Index.md" .
+            echo ".gitkeep" > .gitignore
+            git add Index.md .gitignore
+            git commit -m "New WIKI_HOME created; added default Index.md."
+        )
+        new_wiki_home=x
+    }
+    WikiFEModel FE_MODEL root="$WIKI_HOME"
+
+    main/ id=main data-scope
         div/ id="file-explorer-panel"
 
-            a/ id=$FE_MODEL-new-note   data-scope=file-explorer ws-send href=# ="+ Note"; sp
-            a/ id=$FE_MODEL-new-folder data-scope=file-explorer ws-send href=# ="+ Folder"
+            small/ style="float: right; margin-right: 5px"
+                a/ id=$FE_MODEL-new-note   data-scope=file-explorer ws-send href=# ="+Note"; sp
+                a/ id=$FE_MODEL-new-folder data-scope=file-explorer ws-send href=# ="+Folder"
+            /small
             @file-explorer $FE_MODEL
 
             div/ id=fe-context-menu class=ContextMenu \
@@ -77,6 +86,10 @@ FileExplorerModel FE_MODEL root="$WIKI_HOME"
         /div
         div/ id=file-viewer; /div
     /main
+
+    if [[ ${new_wiki_home:-} ]]; then
+        flash msg="Created WIKI_HOME at: $WIKI_HOME"
+    fi
 }
 
 @handle-file-deletes () {
@@ -86,10 +99,23 @@ FileExplorerModel FE_MODEL root="$WIKI_HOME"
         msg $FE_MODEL id-path $path_id; paths+=("$RESULT")
         msg $FE_MODEL remove-id $path_id
     done
-    (cd "$WIKI_HOME" && rm -rf -- "${paths[@]}")
+    (cd "$WIKI_HOME"
+     rm -rf -- "${paths[@]}"
+     git rm -r --ignore-unmatch -- "${paths[@]}"
+     if git status --porcelain -- "${paths[@]}" | grep -q ^D; then
+         git commit -m "Delete files"$'\n'$'\n'"$(
+             IFS=$'\n'; sed -e 's/^/    /' <<<"${paths[*]}"
+         )"
+     fi
+    )
     for path_id in "${val_path_ids[@]}"; do
         li/ id=$path_id hx-swap-oob=delete; /li
     done
+
+    if [[ " $CURRENT_NODE " == *" ${val_path_ids[*]} "* ]]; then
+        # clear the file-viewer
+        div/ id=file-viewer; /div
+    fi
 }
 
 @handle-file-rename () {
@@ -114,7 +140,16 @@ FileExplorerModel FE_MODEL root="$WIKI_HOME"
         return 0
     }
 
-    (cd "$WIKI_HOME" && mv "$path" "$new_path")
+    (cd "$WIKI_HOME"
+     if git-tracked "$path"; then
+         git mv -k -- "$path" "$new_path"
+         git commit -m "Rename $path to $new_path"
+     else
+         # file is not tracked by git yet; this can happen when a new file is just
+         # created but not yet saved.
+         mv "$path" "$new_path"
+     fi
+    )
 
     # Remove old path-id and re-render from the parent directory
     msg $FE_MODEL remove-id "$val_path_id"
@@ -137,6 +172,7 @@ create-untitled () (  # <file|folder>
                 touch "$name.$ext"
             else
                 mkdir "$name"
+                touch "$name/.gitkeep"
             fi
             return
         fi
@@ -189,9 +225,12 @@ EOF
                     )"
                     return
                 else
+                    # e.g., permission issue, or moving a directory to a non-directory
                     flash msg="Failed moving $src to $dst !"
                 fi
             else
+                -git-mv "${src#$WIKI_HOME/}" "${dst#$WIKI_HOME/}"
+
                 # Remove the moved item because it will get a new id when
                 # listed from its new directory later
                 msg $model remove-id "$tid"
@@ -207,12 +246,35 @@ EOF
     file-explorer "$@"
 }
 
+-git-mv () (
+    cd "$WIKI_HOME" || err-return
+    local src=$1 dst=$2 tracked
+
+    git-tracked "$src" && {
+        git rm "$src" || err-return
+        tracked=x
+    }
+    if [[ -d $dst ]]; then
+        [[ $tracked ]] && { git add "$dst/${src##*/}" || err-return; }
+
+    elif git-tracked "$dst"; then
+        git add "$dst" || err-return
+        tracked=x
+    fi
+    if [[ ${tracked:-} ]]; then
+        git commit -m "Move $src to $dst" || err-return
+    fi
+)
+
 @fe-handle-item-move-overwrite () {
     local src dst tid; get-hx-tid
     local model=${tid%%-*}
     msg $model id-path "$val_fromItemId"; src=$WIKI_HOME/$RESULT
     msg $model id-path "$val_toItemId"; dst=$WIKI_HOME/$RESULT
+
     mv -- "$src" "$dst"
+    -git-mv "${src#$WIKI_HOME/}" "${dst#$WIKI_HOME/}"
+
     li/ id=$val_fromItemId hx-swap-oob=delete; /li
     msg $model remove-id "$val_fromItemId"
 }
@@ -223,14 +285,6 @@ EOF
     file-explorer "$model"
 }
 
-CURRENT_NODE=  # path-id of the current node being viewed
-CURRENT_CONTENTS=
-
-# a stack of path-id's that lead to the currently selected page being viewed;
-# ${LINK_STACK[-1]} is the top.
-#
-LINK_STACK=("")
-
 
 @file-viewer () {  # <path-id> ['view'|'edit']
     local path_id=${1:-${val_path_id:-}} path
@@ -239,9 +293,13 @@ LINK_STACK=("")
 
     local tid; get-hx-tid
     if [[ $tid == back-btn ]]; then
-        path_id=$val_back_btn_link
-        msg $model id-path "$path_id"; path=$RESULT
-        fe-navigate-to-path "$path_id" "$path" "$val_path_id"
+
+        # This check is needed because the node might have been moved or deleted
+        if msg $model has-path-id "$val_back_btn_link"; then
+            path_id=$val_back_btn_link
+            msg $model id-path "$path_id"; path=$RESULT
+            fe-navigate-to-path "$path_id" "$path" "$val_path_id"
+        fi
         (( ${#LINK_STACK[*]} > 1 )) && unset -v "LINK_STACK[-1]"
 
     elif [[ ${val_link:-} ]]; then
@@ -250,7 +308,7 @@ LINK_STACK=("")
             LINK_STACK+=("$val_path_id")
 
     elif [[ ! ${val_path_id:-} ]]; then
-        # user clicekd on a file node in the file explorer
+        # user clicked on a file node in the file explorer
         [[ $CURRENT_NODE && ${LINK_STACK[-1]:-} != "$CURRENT_NODE" ]] &&
             LINK_STACK+=("$CURRENT_NODE")
     fi
@@ -264,7 +322,7 @@ LINK_STACK=("")
             return
         }
         if [[ ! -e "$WIKI_HOME/$path" ]]; then
-            (cd "$WIKI_HOME" && mkdir -p "$(dirname "$path")")
+            (cd "$WIKI_HOME" && d=$(dirname "$path") && mkdir -p "$d" && touch "$d/.gitkeep")
             touch "$WIKI_HOME/$path"
             mode=edit
         fi
@@ -294,6 +352,10 @@ LINK_STACK=("")
 
     if [[ -v val_btn_save ]]; then
         printf -- %s "$CURRENT_CONTENTS" > "$path"
+        (cd "$WIKI_HOME"
+         git add -- "${path#$WIKI_HOME/}"
+         git commit -m "Add or update ${path#$WIKI_HOME/}"
+        )
         [[ $val_btn_save == edit ]] && mode=view
 
     elif [[ -v val_btn_cancel ]]; then
@@ -312,13 +374,15 @@ LINK_STACK=("")
             else
                 button label=Back disabled; sp
             fi
+            # QUESTION: Do we want a Forward button?
+
             case $mode in
                 edit) local btn_mode=view ;;
                 view) local btn_mode=edit ;;
                 *) err-return ;;
             esac
             button name=btn_mode label=${btn_mode^} value=$btn_mode; sp
-            if [[ -v val_btn_mode ]]; then
+            if [[ $mode == @(view|edit) ]]; then
                 button name=btn_save label=Save value=$mode; sp
                 button name=btn_cancel label=Cancel
             else
@@ -333,6 +397,7 @@ LINK_STACK=("")
         else # edit mode
             textfield class=FileViewer-contents name=contents value="$CURRENT_CONTENTS" \
                       multiline ws-send=no
+            #FIXME: this should be below the buttons
         fi
     /div
 }
